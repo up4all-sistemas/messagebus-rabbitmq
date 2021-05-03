0237960afd94bd0be7455e1dfe20dad8c183163d
up4all-sistemas <artur@up4all.com.br>
2021-05-03 12:53:27 -0300

    add mocked classes

---
+++ Up4All.Framework.MessageBus.RabbitMQ/Mocks/RabbitMQSubscribeClientMocked.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Threading.Tasks;
+
+using Up4All.Framework.MessageBus.Abstractions.Enums;
+using Up4All.Framework.MessageBus.Abstractions.Messages;
+using Up4All.Framework.MessageBus.Abstractions.Mocks;
+
+namespace Up4All.Framework.MessageBus.RabbitMQ
+{
+    public class RabbitMQSubscribeClientMocked : MessageBusSubscribeClientMock, IRabbitMQClient, IDisposable
+    {
+        public RabbitMQSubscribeClientMocked() : base()
+        {
+        }
+
+        public override void RegisterHandler(Func<ReceivedMessage, MessageReceivedStatusEnum> handler, Action<Exception> errorHandler, Action onIdle = null, bool autoComplete = false)
+        {            
+        }
+
+        public void Dispose()
+        {            
+        }
+
+        public override Task Close()
+        {         
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Up4All.Framework.MessageBus.RabbitMQ/Mocks/RabbitMQTopicClientMocked.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Up4All.Framework.MessageBus.Abstractions.Messages;
+using Up4All.Framework.MessageBus.Abstractions.Mocks;
+
+namespace Up4All.Framework.MessageBus.RabbitMQ
+{
+    public class RabbitMQTopicClientMocked : MessageBusTopicClientMock, IRabbitMQClient
+    {
+        public RabbitMQTopicClientMocked() : base()
+        {
+        }
+
+        public override Task Send(MessageBusMessage message)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task Send(IEnumerable<MessageBusMessage> messages)
+        {            
+            return Task.CompletedTask;
+        }
+    }
+}
