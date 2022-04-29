fb9d0fd750e86ea04a0a7354ad895697bb2aaa46
Artur Tavares <artur@up4all.com.br>
2022-04-29 13:14:46 -0300

    add logger

--- Up4All.Framework.MessageBus.RabbitMQ/RabbitMQClientExtensions.cs
+++ Up4All.Framework.MessageBus.RabbitMQ/RabbitMQClientExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Polly;
+﻿using Microsoft.Extensions.Logging;
+
+using Polly;
 
 using RabbitMQ.Client;
 using RabbitMQ.Client.Exceptions;
@@ -13,13 +15,19 @@ namespace Up4All.Framework.MessageBus.RabbitMQ
     {
         public static IConnection GetConnection(this IRabbitMQClient client, MessageBusOptions opts)
         {
+            var logger = LoggerFactory
+                    .Create(cfg => { })
+                    .CreateLogger<IRabbitMQClient>();
+
             var result = Policy
                 .Handle<BrokerUnreachableException>()
                 .WaitAndRetry(opts.ConnectionAttempts, retryAttempt => {
-                    TimeSpan wait = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));                    
+                    TimeSpan wait = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
+                    logger.LogInformation($"Failed to connect in RabbitMQ server, retrying in {wait}");
                     return wait;
                 })
                 .ExecuteAndCapture(() => {
+                    logger.LogDebug($"Trying to connect in RabbitMQ server");
                     return new ConnectionFactory() { Uri = new Uri(opts.ConnectionString) }.CreateConnection();
                 });
 
