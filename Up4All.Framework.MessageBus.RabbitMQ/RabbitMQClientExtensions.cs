1779c71ae0cf8439e2e4b3df8f311c130c373cba
Artur Tavares <artur@up4all.com.br>
2022-04-29 12:51:54 -0300

    add polly wait and retry

--- Up4All.Framework.MessageBus.RabbitMQ/RabbitMQClientExtensions.cs
+++ Up4All.Framework.MessageBus.RabbitMQ/RabbitMQClientExtensions.cs
@@ -1,4 +1,7 @@
-﻿using RabbitMQ.Client;
+﻿using Polly;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
 
 using System;
 
@@ -10,7 +13,20 @@ namespace Up4All.Framework.MessageBus.RabbitMQ
     {
         public static IConnection GetConnection(this IRabbitMQClient client, MessageBusOptions opts)
         {
-            return new ConnectionFactory() { Uri = new Uri(opts.ConnectionString) }.CreateConnection();
+            var result = Policy
+                .Handle<BrokerUnreachableException>()
+                .WaitAndRetry(opts.ConnectionAttempts, retryAttempt => {
+                    TimeSpan wait = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));                    
+                    return wait;
+                })
+                .ExecuteAndCapture(() => {
+                    return new ConnectionFactory() { Uri = new Uri(opts.ConnectionString) }.CreateConnection();
+                });
+
+            if (result.Outcome == OutcomeType.Successful)
+                return result.Result;
+
+            throw result.FinalException;
         }
 
         public static IModel CreateChannel(this IRabbitMQClient client, IConnection conn)
